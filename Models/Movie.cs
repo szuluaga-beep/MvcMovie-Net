177472c1e0fb690026830d860e317ab6e53bb168
szuluaga-beep <steven000069@hotmail.com>
2025-07-12 15:43:17 -0500

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
