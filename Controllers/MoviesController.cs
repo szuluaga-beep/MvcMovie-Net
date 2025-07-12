55ea2f530d1e88d3acbfc023898c15d110465b10
szuluaga-beep <steven000069@hotmail.com>
2025-07-12 17:19:37 -0500

    Add movie filtering by genre and title to Index view
    Enhanced the `Index` action in `MoviesController` to support filtering movies by genre and title using a new `MovieGenreViewModel`. Added the `MovieGenreViewModel.cs` file to encapsulate filtering data. Updated the `Index.cshtml` view to include a filtering form and display movies using the view model.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,9 +20,37 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(m => m.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync(),
+            };
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
